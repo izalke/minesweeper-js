9d7af487929dc2dc4e8c4cbf3945dccd049f8fef
Iza Skowrońska <31279258+izalke@users.noreply.github.com>
2024-06-28 10:00:40 +0200

    feat: add styles

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Funkcja do generowania pustej planszy
 const generateClearBoard = (height, width) => {
   let board = [];
 
@@ -9,11 +10,12 @@ const generateClearBoard = (height, width) => {
   }
   return board;
 };
-
+// Funkcja do losowego wybierania liczby od 0 do max-1
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+// Funkcja do umieszczania bomb na planszy
 const populateBoardWithMines = (board, noBombs) => {
   if (board.length == 0) {
     return;
@@ -23,27 +25,32 @@ const populateBoardWithMines = (board, noBombs) => {
   };
   let bombCount = 0;
   while (bombCount != noBombs) {
-    let x = getRandomInt(board[0].length);
-    let y = getRandomInt(board.length);
-    if (board[x][y] == 0) {
-      board[x][y] = -1;
+    let x = getRandomInt(board[0].length); // losowy x
+    let y = getRandomInt(board.length); // losowy y
+    if (board[y][x] == 0) {  // sprawdzenie, czy w tym miejscu nie ma bomby
+      board[y][x] = -1;
       bombCount++;
     }
   }
 };
 
+// Funkcja do rysowania planszy na stronie
 const drawBoard = (board) => {
   const container = document.getElementById("game-container");
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
       let cell = document.createElement("div");
-      cell.id = "${i}-{j}";
+      cell.id = `${i}-${j}`;
       cell.classList.add("boardStyle");
+      if (board[i][j] === -1) {
+        cell.classList.add("bomb");
+      }
       container.appendChild(cell);
     }
   }
 };
 
+// Generowanie i rysowanie planszy
 const mineSweeperBoard = generateClearBoard(10, 10);
 console.log(mineSweeperBoard);
 populateBoardWithMines(mineSweeperBoard, 10);
